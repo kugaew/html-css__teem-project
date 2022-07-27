66225d4ef392465e7718a9bf89df1276e3ad2a6d
kugaew <kugaew@gmail.com>
2022-07-27 10:11:12 +0000

    Deploying to gh-pages from @ kugaew/html-css__teem-project@b89985eae3ebd1ab6661bea487651f0a6b74b0a6 🚀

---
+++ index.67e5fb4b.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector(".js-open-buynow"),closeModalBtn:document.querySelector(".js-close-buynow"),modal:document.querySelector(".buynow__backdrop"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.67e5fb4b.js.map
